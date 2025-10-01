56b7db6f41aafab44da1bcedd08ed1f6024916ed
hyunsuki <81206068+hyunsuki@users.noreply.github.com>
2025-10-01 15:51:39 +0900

    Update page.py

--- page.py
+++ page.py
@@ -6,5 +6,8 @@ import pandas as pd
 # ─────────────────────────────────────────────
 # Streamlit UI
 # ─────────────────────────────────────────────
+
+st.set_page_config(page_title="다이렉트 굿앤굿어린이종합보험Q(태아)", layout="wide")
+
 st.title("보험료 계산 예시 페이지")
 st.write("보험료 계산 예시 페이지입니다!")
