9dade6f03c4600106085be62136038d893b9a88f
hyunsuki <81206068+hyunsuki@users.noreply.github.com>
2025-10-01 15:46:29 +0900

    Create page.py

---
+++ page.py
@@ -0,0 +1,10 @@
+# this is test page
+
+import streamlit as st
+import pandas as pd
+
+# ─────────────────────────────────────────────
+# Streamlit UI
+# ─────────────────────────────────────────────
+st.title("보험료 계산 예시 페이지")
+st.write("보험료 계산 예시 페이지입니다!")
